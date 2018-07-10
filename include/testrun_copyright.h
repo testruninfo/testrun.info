c51e8f9b77f804de07e40303abc3d06e0b66e3e9
Markus Toepfer <Markus.Toepfer@dlr.de>
2018-07-10 14:34:09 +0200

    testrun_copyright rework and retesting done

--- include/testrun_copyright.h
+++ include/testrun_copyright.h
@@ -42,6 +42,24 @@
 
 #include "testrun_log.h"
 
+typedef struct testrun_copyright testrun_copyright;
+
+/*
+ *      ------------------------------------------------------------------------
+ *
+ *      Configuration data for GPL licences.
+ *
+ *      ------------------------------------------------------------------------
+ */
+
+typedef enum {GENERAL, LESSER, AFFERO} testrun_gpl_v3_types;
+
+struct testrun_copyright_gpl_v3_parameter {
+
+        testrun_gpl_v3_types type;
+        char *program_name; 
+        
+};
 
 /*
  *      ------------------------------------------------------------------------
@@ -52,10 +70,11 @@
  *      Simple convention: Function of the testrun copyright structure MUST 
  *      return an allocated zero terminated string, or NULL.
  *
+ *      A backpointer to self is used to check for additional data, stored
+ *      at the data pointer. 
+ *
  *      ------------------------------------------------------------------------
  */
-
-typedef struct testrun_copyright testrun_copyright;
 
 struct testrun_copyright{
 
@@ -66,9 +85,10 @@ struct testrun_copyright{
                                                  const char *end_note, 
                                                  const char *suffix,   
                                                  uint16_t line_indent,
-                                                 bool indent_using_spaces);
+                                                 bool indent_using_spaces,
+                                                 void *custom_data);
 
-        char *(*generate_full_text_licence)     ();
+        char *(*generate_full_text_licence)     (void *custom_data);
 
 };
 
@@ -124,7 +144,7 @@ char *testrun_copyright_generate_header_string(
         ------------------------------------------------------------------------
 
 */
-struct testrun_copyright testrun_copyright_functions_reserved();
+struct testrun_copyright testrun_copyright_reserved();
 
 /**     ------------------------------------------------------------------------
 
@@ -151,7 +171,140 @@ struct testrun_copyright testrun_copyright_functions_reserved();
         ------------------------------------------------------------------------
 
 */
-struct testrun_copyright testrun_copyright_functions_apache_version_2();
+struct testrun_copyright testrun_copyright_apache_version_2();
+
+/**     ------------------------------------------------------------------------
+
+        Function set for a BSD 3-Clause copyright of form:
+
+        ------------------------------------------------------------------------
+
+        Copyright (c) YEAR NAME
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+            * Redistributions of source code must retain the above copyright
+              notice, this list of conditions and the following disclaimer.
+            * Redistributions in binary form must reproduce the above copyright
+              notice, this list of conditions and the following disclaimer in the
+              documentation and/or other materials provided with the distribution.
+            * Neither the name of the copyright holder nor the
+              names of its contributors may be used to endorse or promote products
+              derived from this software without specific prior written permission.
+
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+        DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+        ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+        NOTE
+
+        ------------------------------------------------------------------------
+
+*/
+struct testrun_copyright testrun_copyright_bsd_3clause();
+
+/**     ------------------------------------------------------------------------
+
+        Function set for a BSD 2-Clause copyright of form:
+
+        ------------------------------------------------------------------------
+
+        Copyright (c) YEAR NAME
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this
+           list of conditions and the following disclaimer.
+        2. Redistributions in binary form must reproduce the above copyright notice,
+           this list of conditions and the following disclaimer in the documentation
+           and/or other materials provided with the distribution.
+
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+        ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+        WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+        ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+        (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+        LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+        ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+        NOTE
+
+        ------------------------------------------------------------------------
+
+*/
+struct testrun_copyright testrun_copyright_bsd_2clause();
+
+/**     ------------------------------------------------------------------------
+
+        Function set for a MIT copyright of form:
+
+        ------------------------------------------------------------------------
+
+        Copyright (c) YEAR NAME
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+        NOTE
+
+        ------------------------------------------------------------------------
+
+*/
+struct testrun_copyright testrun_copyright_mit();
+
+/**     ------------------------------------------------------------------------
+
+        Function set for a GPL v3 copyright of form:
+
+        ------------------------------------------------------------------------
+
+        This file is part of [NAME]
+        
+        [NAME] is free software: you can redistribute it and/or modify
+        it under the terms of the GNU [TYPE] Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+        
+        [NAME] is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU [TYPE] Public License for more details."
+
+        You should have received a copy of the GNU [TYPE] Public License
+        along with [PROGRAM_NAME]. If not, see <http://www.gnu.org/licenses/>.
+
+        NOTE
+
+        ------------------------------------------------------------------------
+
+*/
+struct testrun_copyright testrun_copyright_gpl_version_3();
 
 #endif /* testrun_copyright_h */
 
